b4423b0abab77d3e4e0bc4c426fc564adbfae702
Matheus <matheus.santos4@ufv.br>
2026-06-15 23:17:27 -0300

    Change hash table size and update function signatures

--- hash/hash.h
+++ hash/hash.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
-#define M 7
+#define M 509
 #define N 20
 #define TAMALFABETO 256
 
@@ -14,7 +14,6 @@ typedef struct TipoItem {
   int idDoc;
 } TipoItem;
 
-typedef unsigned int TipoIndice;
 typedef struct TipoCelula* TipoApontador;
 typedef struct TipoCelula {
   TipoItem Item;
@@ -29,13 +28,11 @@ typedef TipoLista TipoDicionario[M];
 
 void FLVazia(TipoLista *Lista);
 short Vazia(TipoLista Lista);
-void Ret(TipoApontador p, TipoLista *Lista, TipoItem *Item);
 void GeraPesos(TipoPesos p);
-TipoIndice h(TipoChave Chave, TipoPesos p);
+unsigned int h(TipoChave Chave, TipoPesos p);
 void Inicializa(TipoDicionario T);
 TipoApontador Pesquisa(TipoChave Ch, TipoPesos p, TipoDicionario T);
 void Insere(TipoItem x, TipoPesos p, TipoDicionario T);
-void Retira(TipoItem x, TipoPesos p, TipoDicionario T);
 void Imp(TipoLista Lista);
 void Imprime(TipoDicionario Tabela);
 void LerPalavra(char *p, int Tam);
