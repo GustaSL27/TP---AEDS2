9f24d9d6013b36de48b5d17a7c144b228c5f6b7c
pedromsf <pedromouras1171@gmail.com>
2026-06-18 19:06:22 -0300

    Update patricia.h

--- PATRICIA/patricia.h
+++ PATRICIA/patricia.h
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXPALAVRA 50
+#define MAXPALAVRA 100
+#define MAXDOCS 15
 #define D (MAXPALAVRA * 8)
 
-typedef unsigned char TipoChave[MAXPALAVRA];  // char para bitS
+typedef unsigned char TipoChave[MAXPALAVRA];  // char para bits
 typedef unsigned int TipoIndex;  // indice do bit analisado
 
 // no externo(folha) e a palavra
@@ -20,7 +21,7 @@ typedef enum {
 
 // union guarda dados de no interno ou externo - nunca duas coisas ao mesmo tempo
 // quando interno, guarda o index do bit, ponteiro para o filho esquerdo e ponteiro para o filho direito
-// quando externo, guarda o valor da chave
+// quando externo, guarda o valor da chave, quantas vezes apareceu em cada documento e o identificador do documento
 typedef struct TipoPatNo* TipoArvore;
 
 typedef struct TipoPatNo {
@@ -32,18 +33,19 @@ typedef struct TipoPatNo {
         } NInterno;
         struct {
             TipoChave Chave;
-            int contador;  // quantas vezes a palavra apareceu
+            int qtde[MAXDOCS];  // quantas vezes a palavra apareceu em cada documento
+            int idDoc;          // identificador do documento
         } NExterno;
     } NO;
 } TipoPatNo;
 
-
 int TipoBit(TipoIndex i, TipoChave k);
 short VerificaExterno(TipoArvore p);
 TipoArvore CriaNoInterno(int i, TipoArvore *esq, TipoArvore *dir);
-TipoArvore CriaNoExterno(TipoChave k);
+TipoArvore CriaNoExterno(TipoChave k, int idDoc);
 void Pesquisa(TipoChave k, TipoArvore t);
-TipoArvore InsereEmDivergencia(TipoChave k, TipoArvore *t, int i);
-TipoArvore Insere(TipoChave k, TipoArvore *t);
+TipoArvore InsereEmDivergencia(TipoChave k, TipoArvore *t, int i, int idDoc);
+TipoArvore Insere(TipoChave k, TipoArvore *t, int idDoc);
+void Imprime(TipoArvore t);
 
 #endif
