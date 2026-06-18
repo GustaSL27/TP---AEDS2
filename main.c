2622dde6d80c97138d95cac1d86ff4258bf72c02
Matheus <matheus.santos4@ufv.br>
2026-06-18 20:40:00 -0300

    Implement main function for hash table processing

--- main.c
+++ main.c
@@ -1,3 +1,45 @@
-/*
-Ideia: Fazer uma arvore de pesquisa com as stopwords para melhorar o desempenho na hora de ver se aquela palavra X está na lista
-*/
+#include "hash/hash.c"
+#include "PATRICIA/PATRICIA.c"
+
+int main() {
+
+    TabelaHash Tabela;
+    TipoPesos pesos;
+    Inicializa(Tabela);
+    GeraPesos(pesos);
+
+    FILE *entrada = fopen("../entrada.txt", "r");
+    if (!entrada) {
+        fprintf(stderr, "Erro ao abrir entrada.txt\n");
+        return 1;
+    }
+
+    int qtd;
+    fscanf(entrada, "%d\n", &qtd);
+
+    for (int i = 0; i < qtd; i++) {
+        char nomeFabula[64];
+        fscanf(entrada, "%s", nomeFabula);
+
+        FILE *fabula = fopen(nomeFabula, "r");
+        if (!fabula) {
+            fprintf(stderr, "Erro ao abrir %s\n", nomeFabula);
+            continue;
+        }
+
+        TipoItem x;
+        x.idDoc=i+1;
+        while (fscanf(fabula, "%s", x.Palavra) == 1) {
+            Insere(x, pesos, Tabela);
+        }
+
+        fclose(fabula);
+    }
+
+    fclose(entrada);
+
+    printf("Hash Após entrada:\n\n");
+    Imprime(Tabela);
+
+    return 0;
+}
