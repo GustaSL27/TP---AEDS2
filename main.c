9d477120d3f90385cbc513b3c573a7fb8683e1e6
GustaSL27 <gustavosl.2007@gmail.com>
2026-06-23 02:27:36 -0300

    Enhance hash table insertion and printing functionality
    Added functionality to track total elements in the hash table and updated related function calls.

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ int main() {
     clock_t tempo_inicio_PATRICIA, tempo_final_PATRICIA;
     double tempo_total_Hash = 0;
     double tempo_total_PATRICIA = 0;
+    int total_elementos_array = 0;
 
     TabelaHash Tabela;
     TipoPesos pesos;
@@ -24,6 +25,7 @@ int main() {
 
     printf("Bem vindo ao sistema de indexacao de fabulas\n\n");
 
+    // Começo do menu
     do {
         printf("\nQual funcao voce gostaria de usar:\n\n");
         printf("a - Receber e inserir as fabulas\n");
@@ -33,16 +35,19 @@ int main() {
         printf("Digite a letra da funcao desejada: ");
         scanf(" %c", &decisao);
 
+
+        // Escolhe inserir
         if(decisao == 'a'){
             LerStopWord(nomeArquivoStop, &arvorestop);
 
+            // Salva a quantidade de fábulas na variável qtd
             int qtd = LerEntrada("fabulas/entrada.txt");
             if (qtd == -1) {
                 printf("Erro ao ler entrada.txt\n");
             }
 
             do{
-                printf("\nOnde você gostaria de inserir as fábulas:\n");
+                printf("\nOnde voce gostaria de inserir as fabulas:\n");
                 printf("\n1- Hash\n");
                 printf("2- Patricia\n");
                 printf("Digite a opcao desejada: ");
@@ -57,7 +62,7 @@ int main() {
                 printf("Inserindo na tabela hash...\n\n");
 
                 tempo_inicio_Hash = clock();
-                LerFabulasHash("fabulas/fabula", qtd, arvorestop, Tabela, pesos);
+                LerFabulasHash("fabulas/fabula", qtd, arvorestop, Tabela, pesos, &total_elementos_array);
                 tempo_final_Hash = clock();
 
                 double tempo_total_Hash = (double)(tempo_final_Hash - tempo_inicio_Hash) / CLOCKS_PER_SEC;
@@ -74,6 +79,8 @@ int main() {
                 printf("Insercao PATRICIA concluida\n\n");
             }
         }
+
+        // Escolhe imprimir (ordem alfabetica + estatistica de tempo)
         else if (decisao == 'b') {
             do
             {
@@ -91,7 +98,7 @@ int main() {
             if(opcao == 1){
                 printf("Imprimindo tabela hash...\n\n");
                 printf("Hash apos entrada:\n\n");
-                ImprimeHash(Tabela);
+                ImprimeArrayOrdenado(total_elementos_array);
                 printf("Imprimindo tempo da tabela hash...\n\n");
                 printf("%.5f segundos", tempo_total_Hash);
             }
