382ae666076996645114cfc3fb82cc611b21e699
GustaSL27 <gustavosl.2007@gmail.com>
2026-06-23 02:27:19 -0300

    Enhance hash.h with array functions and InsereHash update
    Added new functions for array handling and modified InsereHash to include total count.

--- hash/hash.h
+++ hash/hash.h
@@ -7,9 +7,11 @@
 #define M 509
 #define N 20
 #define TAMALFABETO 256
+#define MAX_ARRAY 5000
 
 typedef char TipoPalavra[N];
 typedef unsigned TipoPesos[N][TAMALFABETO];
+
 typedef struct TipoItem {
   TipoPalavra Palavra;
   int qnt;
@@ -28,13 +30,20 @@ typedef struct ListaEncadeada {
 
 typedef ListaEncadeada TabelaHash[M];
 
+// Vetor contendo as palavras para ordenar
+TipoItem Palavras_ordenadas[MAX_ARRAY];
+
+void InsereArray(TipoItem item, int *total);
+void ImprimeArrayOrdenado();
+int comparar_elementos_vetor(const void *a, const void *b);
+
 void FLVazia(ListaEncadeada *Hash);
 short Vazia(ListaEncadeada Hash);
 void GeraPesos(TipoPesos p);
 unsigned int h(TipoPalavra Palavra, TipoPesos p);
 void Inicializa(TabelaHash T);
 TipoApontador PesquisaHash(TipoPalavra Ch, TipoPesos p, TabelaHash T);
-void InsereHash(TipoItem x, TipoPesos p, TabelaHash T);
+void InsereHash(TipoItem x, TipoPesos p, TabelaHash T, int *total);
 void Imp(ListaEncadeada Hash);
 void ImprimeHash(TabelaHash Tabela);
 void LerPalavra(char *p, int Tam);
