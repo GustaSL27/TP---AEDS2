ce9745289991596af0a23b409577f0254b0a72fb
GustaSL27 <gustavosl.2007@gmail.com>
2026-06-23 21:36:30 -0300

    Add total parameter to LerFabulasHash function

--- leitura/leitura.h
+++ leitura/leitura.h
@@ -5,7 +5,8 @@
 #include "../hash/hash.h"
 
 int LerEntrada(char * entrada);
+void LerFabulas(char *nomeArquivoBase, int qtd, TipoArvore *arvore, TipoArvore arvorestop, TabelaHash Tabela, TipoPesos pesos);
 void LerFabulasPATRICIA(char *nomeArquivoBase, int qtd, TipoArvore *arvore, TipoArvore arvorestop);
-void LerFabulasHash(char *nomeArquivoBase, int qtd, TipoArvore arvorestop, TabelaHash Tabela, TipoPesos pesos);
+void LerFabulasHash(char *nomeArquivoBase, int qtd, TipoArvore arvorestop, TabelaHash Tabela, TipoPesos pesos, int *total);
 
 #endif
